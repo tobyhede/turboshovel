061b74529ad2e4e7337dc2a46e00bf6b82e8740c
Toby Hede <toby@cipherstash.com>
2025-12-24 09:39:16 +1100

    fix(cli): remove unused parseTaskId import

--- plugin/hooks/hooks-app/src/cli/workflow-cli.ts
+++ plugin/hooks/hooks-app/src/cli/workflow-cli.ts
@@ -6,7 +6,7 @@ import * as fs from 'fs/promises';
 import * as path from 'path';
 import { WorkflowStateManager } from '../workflow/state';
 import { parseWorkflow, WorkflowSyntaxError } from '../workflow/parser';
-import { parseTaskId, taskIdToString, type TaskId } from '../workflow/task-id';
+import { taskIdToString, type TaskId } from '../workflow/task-id';
 import { createTaskNumber, type Action, type Task } from '../workflow/types';
 
 const program = new Command();
